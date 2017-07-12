e4d413b124a4bd955b890062b0181c73df43e07b
Eduardo <jssedlr@gmail.com>
2017-07-12 13:31:58 -0600

    Listo
    * Funcionalidad con jQuery

--- js/app.js
+++ js/app.js
@@ -1,93 +1,124 @@
-//INFORMACION ESTUDIANTES
-var estudiantes = [
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0},
-  {"codigo":"","nombre":"","nota":0}
-];
-// FUNCIONES DE BOTONES
-var index = 0;
-function boton1() {
-  if (index < 10) {
-    estudiantes[index].codigo = document.getElementById("codigo").value 
-    estudiantes[index].nombre = document.getElementById("nombre").value 
-    estudiantes[index].nota = document.getElementById("nota").value
-    imprimirDatosEst(index);
-    index++
-  }else{
-    alert("El número máximo de estudiantes es 10.")
+//FUNCIONES CON LOCALSTORAGE
+function editNota(codeKey){
+  var est;
+  for (var i = 0; i < localStorage.length; i++) {
+    var clave = localStorage.key(i);
+    if(clave == codeKey){
+      est = $.parseJSON(localStorage.getItem(clave));
+      $("#id").val(est.id);
+      $("#nota").val(est.nota);
+      $("#nombre").val(est.nombre);
+    }
   }
+}; 
+
+function deleteNota(codeKey){
+  localStorage.removeItem(codeKey);
+  listNotes();
 };
 
+function listNotes(){
+  var tabla = "";
+  var rowData = $("#tbody");
+
+  for (var i = 0; i < localStorage.length; i++) {
+    var clave = localStorage.key(i);
+    var est = $.parseJSON(localStorage.getItem(clave))
+    tabla += "<tr>";
+    tabla += "<td>"+est.id+"</td>";
+    tabla += "<td>"+est.nombre+"</td>";
+    tabla += "<td class='scores'>"+est.nota+"</td>";
+    tabla += '<td><button class="boti" onclick="editNota(\'' + est.id + '\')">Editar</button></td> ';
+    tabla += '<td><button class="boti" onclick="deleteNota(\'' + est.id + '\')">Eliminar</button></td> ';
+    tabla += "</tr>";
+  };
+
+  $(rowData).html(tabla);
+};
+
+//FUNCIONES PARA PROCESO DE DATOS
 function boton2(){
   var promedio = 0;
-  for (var i = 0; i < estudiantes.length; i++) {
-    promedio = promedio + Number(estudiantes[i].nota);
-  };
+  var index = 0;
+  $('.scores').each(function() {
+    promedio += Number($(this).html());
+    index++
+  });
   alert( "El promedio del salon es de "+ (promedio/ index) );
 };
 
 function boton3(){
   var nota = 0,
       est = "";
-  for (var i = 0; i < index; i++) {
-    if ( Number(estudiantes[i].nota) > nota) {
-      est = 'Estudiante '+estudiantes[i].codigo+' - '+
-      'Nombre: '+estudiantes[i].nombre+' - '+'Nota: '+estudiantes[i].nota;
-      nota = Number(estudiantes[i].nota);
+  $('.scores').each(function() {
+    if ( Number($(this).html()) > nota) {
+      est = 'Estudiante '+$(this).parent().find(":nth-child(1)").html()+' - '+
+      'Nombre: '+$(this).parent().find(":nth-child(2)").html()+' - '+'Nota: '+$(this).html();
+      nota = Number($(this).html());
     }
-  }   
-  alert( "La nota mayor corresponde a: \n" +est );
+  });   
+  alert("La nota mayor corresponde a: \n"+est);
 };
 
 function boton4(){
-  var nota = 100,
+  var nota = 101,
       est = "";
-  for (var i = 0; i < index; i++) {
-    if ( Number(estudiantes[i].nota) < nota) {
-      est = 'Estudiante '+estudiantes[i].codigo+' - '+
-      'Nombre: '+estudiantes[i].nombre+' - '+'Nota: '+estudiantes[i].nota;
-      nota = Number(estudiantes[i].nota);
+  $('.scores').each(function() {
+    if ( Number($(this).html()) < nota) {
+      est = 'Estudiante '+$(this).parent().find(":nth-child(1)").html()+' - '+
+      'Nombre: '+$(this).parent().find(":nth-child(2)").html()+' - '+'Nota: '+$(this).html();
+      nota = Number($(this).html());
     }
-  }
-  alert( "La menor nota corresponde a: \n"+est );
-};
-
-//AUX BOTON 1 FUNCT
-function imprimirDatosEst(i) {
-    var node = document.createElement("TR");          
-    var cell1 = document.createElement("TD");          
-    var cell2 = document.createElement("TD");          
-    var cell3 = document.createElement("TD");          
-    var text1 = document.createTextNode(estudiantes[i].codigo);      
-    var text2 = document.createTextNode(estudiantes[i].nombre);      
-    var text3 = document.createTextNode(estudiantes[i].nota);      
-    node.appendChild(cell1);                           
-    node.appendChild(cell2);                           
-    node.appendChild(cell3);                           
-    cell1.appendChild(text1);
-    cell2.appendChild(text2);
-    cell3.appendChild(text3);
-    
-    document.getElementById("tbody").appendChild(node);  
+  });   
+  alert("La nota mayor corresponde a: \n"+est);
 };
 
 //EVENTOS CLICK
-document.getElementsByTagName("button")[0].addEventListener("click", function(){
-  boton1();
-});
-document.getElementsByTagName("button")[1].addEventListener("click", function(){
+
+$("#boton2").on("click", function(){
   boton2();
 });
-document.getElementsByTagName("button")[2].addEventListener("click", function(){
+$("#boton3").on("click", function(){
   boton3();
 });
-document.getElementsByTagName("button")[3].addEventListener("click", function(){
+$("#boton4").on("click", function(){
   boton4();
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//ONLOAD
+$(function(){
+  var count;
+  if (localStorage.length>0) {
+    count = localStorage.length + 1
+  }else{
+    count = 1;
+  }
+  $("#id").val(count);
+  $("#boton1").click(function(){
+    var id = $("#id").val();
+    var nombre = $("#nombre").val();
+    var nota = $("#nota").val();
+
+    var est = {
+      "id": id,
+      "nombre": nombre,
+      "nota": nota
+    };
+
+    localStorage.setItem(id,JSON.stringify(est));
+    count = localStorage.length + 1;
+
+    listNotes();
+    resett();
+  });
+
+  function resett(){
+    $("#id").val(count);
+    $("#nombre").val("");
+    $("#nota").val("");
+  };
+
+  listNotes();
+  $("nota").val();
 });
